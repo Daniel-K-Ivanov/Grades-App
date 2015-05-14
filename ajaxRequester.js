08f49db61b6698b844be40c78f3bf9554792243c
Daniel-K-Ivanov <Daniel.K.Ivanov95@gmail.com>
2015-05-15 02:49:50 +0300

    commit grades app files

---
+++ ajaxRequester.js
@@ -0,0 +1,79 @@
+﻿var ajaxRequester = (function () {
+    var baseUrl = "https://api.parse.com/1/";
+    var headers = {
+        "X-Parse-Application-Id": "20j1lyfIpuRrr4vcMMneYmWiOskc8d9YtEVNIAeW",
+        "X-Parse-REST-API-Key": "TSkdI6NxUabmCKGLPf2KQC5OuIFIcgEqUgrnafnm"
+    };
+
+    function login(username, password, success, error) {
+        jQuery.ajax({
+            method: "GET",
+            headers: headers,
+            url: baseUrl + "login",
+            data: { username: username, password: password },
+            success: success,
+            error: error
+        });
+    };
+
+    function register(username, password, success, error) {
+        jQuery.ajax({
+            method: "POST",
+            headers: headers,
+            url: baseUrl + "users",
+            data: JSON.stringify({ username: username, password: password }),
+            success: success,
+            error: error
+        });
+    };
+
+
+    function getGrades(sessionToken, success, error) {
+        var headersWithSessionToken = getHeadersWithSessionToken(sessionToken);
+        $.ajax({
+            method: "GET",
+            headers: headersWithSessionToken,
+            url: baseUrl + "classes/Grades",
+            success: success,
+            error: error
+        });
+    };
+
+    function getHeadersWithSessionToken(sessionToken) {
+        var headersWithToken = JSON.parse(JSON.stringify(headers));
+        headersWithToken['X-Parse-Session-Token'] = sessionToken;
+        return headersWithToken;
+    };
+
+    function createGrade(subject, grade, userId, success, error) {
+        var mark = { Subject: subject, Grade: Number(grade), ACL: {} };
+        mark.ACL[userId] = { "write": true, "read": true };
+        jQuery.ajax({
+            method: "POST",
+            headers: headers,
+            url: baseUrl + "classes/Grades",
+            data: JSON.stringify(mark),
+            success: success,
+            error: error
+        });
+    }
+
+    function removeGrade(sessionToken, gradeId, success, error) {
+        var headersWithToken = getHeadersWithSessionToken(sessionToken);
+        jQuery.ajax({
+            method: "DELETE",
+            headers: headersWithToken,
+            url: baseUrl + "classes/Grades/" + gradeId,
+            success: success,
+            error: error
+        });
+    }
+
+    return {
+        login: login,
+        register: register,
+        getGrades: getGrades,
+        createGrade: createGrade,
+        removeGrade: removeGrade
+    };
+})();
---
+++ jsApp.js
@@ -0,0 +1,143 @@
+﻿(function () {
+    $(function () {
+        registerEventHandlers();
+        var currentUser = userSession.getCurrentUser();
+        if (currentUser) {
+            showGradesView()
+        } else {
+            showHomeView();
+        }
+
+    });
+    function registerEventHandlers() {
+        $("#btnShowLoginView").click(showLoginView);
+        $("#btnShowRegisterView").click(showRegisterView);
+        $("#btnLogin").click(login);
+        $("#btnRegister").click(register);
+        $('#addBtn').click(addGrade);
+    }
+
+    function showHomeView() {
+        $("main > *").hide();
+        $("#homeView").show();
+        $('header span').text('Grades App').attr('style', 'padding-left:0px');
+    }
+
+    function showLoginView() {
+        $("main > *").hide();
+        $("#loginView").show();
+    }
+
+    function showRegisterView() {
+        $("main > *").hide();
+        $("#registerView").show();
+    }
+
+    function login() {
+        var username = $("#txtLoginUsername").val();
+        var password = $("#txtLoginPassword").val();
+        ajaxRequester.login(username, password, authSuccess, loginError);
+    }
+
+    
+    function register() {
+        var username = $("#txtRegisterUsername").val();
+        var password = $("#txtRegisterPassword").val();
+        ajaxRequester.register(username, password,
+            function (data) {
+                data.username = username;
+                authSuccess(data);
+            },
+            loginError);
+    }
+
+    function loginError() {
+        alert("Login failed.");
+    }
+
+    function authSuccess(data) {
+        userSession.login(data);
+        showGradesView();
+    }
+
+    function showGradesView() {
+        var currentUser = userSession.getCurrentUser(); 
+        if (currentUser) {
+            $("main > *").hide();
+            $('header span').text(currentUser.username + '`s grades').attr('style','padding-left:150px');
+            $('<a href="#">').attr('id', "logout").attr('style', 'float:right').text("Logout").click(logout).appendTo("header span");
+            var sessionToken = currentUser.sessionToken;
+            ajaxRequester.getGrades(sessionToken, loadGradesSuccess,
+                function myfunction() {
+                    alert("Failed to load grades.");
+                }
+            );
+        } else {
+            showHomeView();
+        }
+    }
+
+    function loadGradesSuccess(data) {
+        var $gradesUl = $("#gradesView ul");
+        $gradesUl.html('');
+        for (var g in data.results) {
+            var grade = data.results[g];
+            $('<li>').attr('id', g).text(grade.Subject + " - " + grade.Grade).data("grade", grade).appendTo("#gradesUl");
+            $('<a href="#">').attr('id', g).text("delete").attr('style','margin-left:35px').click(removeGrade).appendTo("#"+g);
+            $('<br />').appendTo("#gradesUl");
+        }
+        $('#gradesView').show();
+    }
+
+    function errorLoadGrade() {
+        alert("fail");
+    };
+
+    //ADD Grade
+    function addGrade() {
+        var subject = $('#subject').val();
+        var grade = $('#grade').val();
+        if (subject != "" && isNaN(subject) && !isNaN(grade)) {
+            var currentUser = userSession.getCurrentUser();
+            ajaxRequester.createGrade(subject, grade, currentUser.objectId, showGradesView,
+                function errorAddGrade() {
+                    alert("failed to add bookmark");
+                })
+        }
+        else {
+            alert("Invalid operation.");
+        }
+        clearInput();
+    }
+
+    function errorAddGrade() {
+        alert("failed to add bookmark");
+    }
+
+    function logout() {
+        userSession.logout();
+        showHomeView();
+        clearInput();
+    }
+
+    function clearInput() {
+        $("#txtLoginUsername").val('');
+        $("#txtLoginPassword").val('');
+        $("#txtRegisterUsername").val('');
+        $("#txtRegisterPassword").val('');
+        $('#subject').val('');
+        $('#grade').val('');
+    }
+
+    //Remove Grade
+    function removeGrade() {
+        var grade = $(this).parent().data('grade');
+        var currentUser = userSession.getCurrentUser();
+        var sessionToken = currentUser.sessionToken;
+        ajaxRequester.removeGrade(sessionToken, grade.objectId, showGradesView,
+            function () {
+                alert("Delete failed.")
+            });
+    }
+
+})();
